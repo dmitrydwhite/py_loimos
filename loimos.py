75f092d9b93037132cea2334c8b684b9d576f25b
Dmitry White <dmitry.white@nike.com>
2015-12-08 16:57:53 -0800

    Game almost completely initialized

--- loimos.py
+++ loimos.py
@@ -10,9 +10,12 @@ from random import shuffle
 class GameSpace:
   MAX_OUTBREAKS = 7
   NUMBER_OF_DISEASES = 4
+  NUMBER_OF_EPIDEMICS = 4
   DISEASE_COLORS = ["yellow", "red", "blue", "gray"]
   DISEASE_CUBES = 24
   STARTING_STATION = "ATLANTA"
+  INFECTION_RATE_INCREMENT = 1
+  game_status = 0
 
   "initialize GameSpace values"
   def __init__(self):
@@ -29,14 +32,19 @@ class GameSpace:
     self.infected_cities = []
 
     self.setup_game()
-    self.start_game()
+    self.play_game()
 
   ## Begin the Game ##
   
   def setup_game(self):
-    pass
-  
-  def start_game(self):
+    self.build_station(self.STARTING_STATION)
+    self.create_the_emergency()
+    for player in self.players:
+      self.move_p(self.players[player], self.players[player]["location"])
+    self.deal_cards()
+    self.insert_epidemics()
+
+  def play_game(self):
     pass
 
   ## Primary GameSpace Initializing Methods ##
@@ -94,7 +102,38 @@ class GameSpace:
     return counter
 
   def init_player_deck(self):
-    pass
+    stack = len(self.cities)
+    player_deck = random.sample(self.cities, stack)
+
+    with open("classic_events.json") as events:
+      grants_json = json.load(events)
+      grants = grants_json["events"]
+
+    player_deck += grants
+
+    random.shuffle(player_deck)
+
+    return player_deck
+
+  def deal_cards(self):
+    num_map = [0, 4, 4, 3, 2, 1, 1, 1]
+    starting_cards = num_map[len(self.players)]
+
+    for player in self.players:
+      self.players[player]["research"] = self.player_dex[0:starting_cards]
+      del self.player_dex[0:starting_cards]
+
+  def insert_epidemics(self):
+    stack = len(self.player_dex)
+    divisions = stack / self.NUMBER_OF_EPIDEMICS
+    stack_start = 0
+    stack_end = divisions
+
+    for count in range(1, self.NUMBER_OF_EPIDEMICS):
+      epidemic_loc = randint(stack_start, stack_end)
+      self.player_dex.insert(epidemic_loc, "EPIDEMIC")
+      stack_start += divisions
+      stack_end += divisions
 
   def init_infection_deck(self):
     infection_dex = random.sample(self.cities, len(self.cities))
@@ -131,10 +170,37 @@ class GameSpace:
 
     return cits
 
-  ## Gameplay Methods ##
+  """
+  This method creates the initial infection state for the starting cities
+  """
+  def create_the_emergency(self):
+    for rate in range(1,4):
+      this_round = self.infection_dex[0:3]
+      del self.infection_dex[0:3]
+      for city in this_round:
+        disease = self.cities[city]['group']
+        infections = {}
+        infections[disease] = rate
+        self.cities[city]['infections'] = infections
+      self.infected_cities += this_round
+      
+  ## Gameplay Player Methods ##
   
   def move_p(self, player, location):
-    pass
+    if player["protect_connected_cities"] == True:
+      self.quarantine_surrounding(player["location"], False)
+
+    player["location"] = location
+
+    if player["protect_connected_cities"] == True:
+      self.quarantine_surrounding(player["location"], True)
+
+    if player["treat_all_on_enter"] == True:
+      if "infections" in self.cities[player["location"]]:
+        for disease in self.cities[player["location"]]["infections"]:
+          if self.diseases[disease]["cured"] == True:
+            self.cities[player["location"]]["infections"][disease] = 0
+
 
   def transfer_research(self, player1, player2, research):
     pass
@@ -144,6 +210,60 @@ class GameSpace:
 
   def cure(self, disease):
     pass
+
+  def build_station(self, location):
+    self.cities[location]["has_station"] = True
+
+  def quarantine_surrounding(self, location, protect):
+    for city in self.cities[location]["connections"]:
+        self.cities[city]["is_quarantined"] = protect
+    self.cities[location]["is_quarantined"] = protect
+
+  ## Gameplay Game Methods ##
+  
+  def epidemic(self):
+    self.increase_rate()
+    self.intensify()
+    self.infect_cities()
+
+  def increase_rate(self):
+    self.infection_rate += self.INFECTION_RATE_INCREMENT
+
+  def intensify(self):
+    new_location = self.infection_dex.pop()
+    self.infect(new_location, 3)
+
+  def infect(self, city_name, cubes, origin_city):
+    if cubes is None:
+      cubes = 1
+
+    if self.cities[city_name]["is_quarantined"]:
+      return
+
+    if not "infectinons" in self.cities[city_name]:
+      self.cities[city_name]["infections"] = {}
+
+    if origin_city is not None:
+      infecting_disease = self.cities[origin_city]["group"]
+    else:
+      infecting_disease = self.cities[city_name]["group"]
+
+    if not infecting_disease in self.cities[city_name]["infections"]:
+      self.cities[city_name]["infections"][infecting_disease] = 0
+
+    self.cities[city_name]["infections"][infecting_disease] += cubes
+
+    if self.cities[city_name]["infections"][infecting_disease] > 3:
+      self.outbreak_from(city_name, infecting_disease)
+
+  def outbreak_from(self, origin_city, disease):
+    self.outbreak_counter["outbreaks"] += 1
+
+    if self.outbreak_counter["outbreaks"] > self.outbreak_counter["maximum"]:
+      self.game_status(1)
+
+    for city_name in self.cities[origin_city]["connections"]:
+      self.infect(city_name, 1, disease)
 
   ## Helper Functions ##
   
@@ -178,7 +298,8 @@ class Player:
     "transfer_any": False,
     "send_others": False,
     "re_apply_grant": False,
-    "protect_connected_cities": False
+    "protect_connected_cities": False,
+    "location": "ATLANTA"
   }
 
   disease = {}
@@ -214,7 +335,8 @@ class Player:
 
 
 L = GameSpace()
+print(L.infected_cities)
+print(L.player_dex)
 
-print(L)
-print(L.players)
-print(L.infection_dex)
+for player in L.players:
+  print(player, L.players[player]["research"])
